82d330e54b186eec7f1e7a9546f851c7574dfbb3
Emre <50625747+memrearal@users.noreply.github.com>
2021-05-31 16:22:35 +0300

    Update 3.py

--- etap1/3.py
+++ etap1/3.py
@@ -1,5 +1,7 @@
 import re
+
 matrix = []
+
 def matrixYukle():
 	with open("oruntumatrisi.txt") as f:
 		for satir in f.readlines():
@@ -26,12 +28,6 @@ def column(matrix):
 		columnler.append([row[i] for row in matrix])
 	return columnler
 
-def solKosegen(matrix):
-	liste = []
-	for i in range(len(matrix[0])):
-		liste.append(matrix[i][i])
-	return liste
-
 def matrixDikey(matrix, oruntu):
 	dikeyler = column(matrix)
 	patternler = []
@@ -45,15 +41,24 @@ def matrixDikey(matrix, oruntu):
 	return patternler
 
 def matrixCapraz(matrix, oruntu):
-	capraz = solKosegen(matrix)
-	patternler = []
-	tryHard="".join(capraz)
-	bulus = [[m.start(0), m.end(0)-1] for m in re.finditer(oruntu, tryHard)]
-	return bulus
+	caprazOruntuler = []
+	for x in range(len(matrix)-(len(oruntu)-1)):
+		for y in range(len(matrix[0])-(len(oruntu)-1)):
+			kontrol=""
+			for i in range(len(oruntu)):
+				kontrol += matrix[x+i][y+i]
+			noktalar = []
+			for i in range(len(oruntu)):
+				nokta = (x+i)*len(matrix[0]) + y+i + 1
+				noktalar.append(nokta)
+			if kontrol == oruntu:
+				caprazOruntuler.append(noktalar)
+	return caprazOruntuler
 
 def test_case(matrix):
 	matrixYukle()
 	oruntu = input("Örüntü: ")
+
 	yatayOruntuler = []
 	for yat in matrixYatay(matrix, oruntu):
 		for elem in yat:
@@ -63,17 +68,6 @@ def test_case(matrix):
 				orR.append(x+i)
 			yatayOruntuler.append(orR)
 	dikeyOruntuler = []
-
-	caprazOruntuler = []
-	for cap in matrixCapraz(matrix, oruntu):
-		baslangic = ((cap[0]) * len(matrix[0])) + cap[0]+1
-		orR = []
-		for i in range(len(oruntu)):
-			deger = baslangic+(i*(len(matrix[0])+1))
-			orR.append(deger)
-		caprazOruntuler.append(orR)
-
-
 	for dik in matrixDikey(matrix, oruntu):
 		for elem in dik:
 			x = elem[0]
@@ -84,10 +78,12 @@ def test_case(matrix):
 			for i in range(len(oruntu)):
 				orR.append(baslangic+i*len(matrix[0]))
 			dikeyOruntuler.append(orR)
+	caprazOruntuler = matrixCapraz(matrix,oruntu)
 	for k,oruntuler in enumerate(yatayOruntuler+dikeyOruntuler+caprazOruntuler):
 		oruntuX = ""
 		for el in oruntuler:
 			oruntuX += "["+str(el)+"]"
 		print("{}. bulunan örüntü: {}".format(k+1,oruntuX))
 	print("Toplam {} tane örüntü bulundu.".format(len(yatayOruntuler+dikeyOruntuler+caprazOruntuler)))
+
 test_case(matrix)
